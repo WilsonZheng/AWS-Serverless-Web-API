afef994653514108f006d0cd9fa4904bbbf93306
Wilson <zhwzhw29@gmail.com>
2017-11-16 21:51:41 +1300

    update

--- WeatherInAuckland/function.js
+++ WeatherInAuckland/function.js
@@ -1,39 +1,42 @@
 'use strict';
 const fetch = require('node-fetch');
 module.exports.getWeather = (event, context, callback) => {
-    /*
-    In serverless.yml , this function would be exported as
-    `getWeather`.
-    */
+  /*
+  In serverless.yml , this function would be exported as
+  `getWeather`.
+  */
 
-    // const data = { temperature: 19.2 };
-    /* Define some dummy data, for now.*/
-    // const response = {
-    //     statusCode: 200,
-    //     headers: {
-    //         "Access-Control-Allow-Origin": "*",
-    //     },
-    //     body: JSON.stringify(data)
-    //     // JSON.stringify converts the JavaScript response object
-    //     // into a JSON string, which is what our client is expecting.
-    //     // Because we'll be depending on CORS we also need to set
-    //     // the `Access-Control-Allow-Origin` header.
+  // const data = { temperature: 19.2 };
+  /* Define some dummy data, for now.*/
+  // const response = {
+  //     statusCode: 200,
+  //     headers: {
+  //         "Access-Control-Allow-Origin": "*",
+  //     },
+  //     body: JSON.stringify(data)
+  //     // JSON.stringify converts the JavaScript response object
+  //     // into a JSON string, which is what our client is expecting.
+  //     // Because we'll be depending on CORS we also need to set
+  //     // the `Access-Control-Allow-Origin` header.
 
-    // };
-    // callback(null, response);
-    
-    // OpenWeatherMap API endpoint.
-    //console.log(process.env.APPID);
-    let id = process.env.APPID;
-    const endpoint = 'http://api.openweathermap.org/data/2.5/weather?q=auckland,nz&units=metric&appid='+ id;
-    
-    fetch(endpoint)
-    .then( res => res.json() )
-    .then( body =>  {
+  // };
+  // callback(null, response);
+
+  // OpenWeatherMap API endpoint.
+  //console.log(process.env.APPID);
+  console.log(JSON.stringify(event));
+  let id = process.env.APPID;
+  let ct = event.city;
+  let country = event.country;
+  const endpoint = 'http://api.openweathermap.org/data/2.5/weather?q=' + ct + ',' + country + '&units=metric&appid=' + id;
+
+  fetch(endpoint)
+    .then(res => res.json())
+    .then(body => {
       const response = {
         statusCode: 200,
         headers: {
-          "Access-Control-Allow-Origin" : "*",
+          "Access-Control-Allow-Origin": "*",
         },
         body: JSON.stringify({ temperature: body.main.temp })
       };
@@ -41,6 +44,6 @@ module.exports.getWeather = (event, context, callback) => {
       callback(null, response);
     });
 
-   
-    
+
+
 }
