0946e16bd13038743ea5fdd310ae7f1cf85e5361
Wilson Zheng <ZhengW@ad.fronde.com>
2017-11-21 14:51:26 +1300

    update

--- Website/fetchData.js
+++ Website/fetchData.js
@@ -1,22 +1,31 @@
+function degToCompass(num) {
+    val = Math.floor((num / 22.5) + 0.5);
+    arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
+    console.log()
+    return arr[(val % 16)];
+}
+
 function renderTemp(data) {
+    
 
     var el = document.getElementById('aklTemp');
-    el.innerHTML = data.temperature + " °C";
+    el.innerHTML = data.temperature + "°C";
 
     var el2 = document.getElementById('aklHumidity');
     el2.innerHTML = data.humidity + "%";
 
     var el3 = document.getElementById('aklWindSpeed');
-    el3.innerHTML = data.windspeed + "m/s";
+    el3.innerHTML = data.windspeed * 3.6 + "km/h";
 
     var el4 = document.getElementById('aklWindDegree');
-    el4.innerHTML = data.windDegree + "°";
-    
+    var direction = degToCompass(data.windDegree);
+    el4.innerHTML =  direction;
+
     var el5 = document.getElementById('aklWeather');
     var iconUrl = "http://openweathermap.org/img/w/" + data.weatherIcon + ".png";
     el5.src = iconUrl;
-    
-    
+
+
 
 }
 
