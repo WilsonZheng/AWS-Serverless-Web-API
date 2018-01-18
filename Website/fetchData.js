5a5e507bd41cee85382a99165c52df296543352a
Wilson Zheng <ZhengW@ad.fronde.com>
2018-01-19 09:11:36 +1300

    update

--- Website/fetchData.js
+++ Website/fetchData.js
@@ -27,44 +27,44 @@ function renderTemp(data) {
     var secondDayIcon = document.getElementById('secondWeatherIcon');
     var secondDayMax = document.getElementById('secondMax');
     var secondDayMin = document.getElementById('secondMin');
-    secondDayMax.innerHTML = data[2].max + "°C";
-    secondDayMin.innerHTML = data[2].min + "°C";
+    secondDayMax.innerHTML = Math.round(data[2].max) + "°C";
+    secondDayMin.innerHTML = Math.round(data[2].min) + "°C";
     secondDayIcon.src = "http://openweathermap.org/img/w/" + data[2].weatherIcon + ".png";
 
     var thirdDayIcon = document.getElementById('thirdWeatherIcon');
     var thirdDayMax = document.getElementById('thirdMax');
     var thirdDayMin = document.getElementById('thirdMin');
-    thirdDayMax.innerHTML = data[3].max + "°C";
-    thirdDayMin.innerHTML = data[3].min + "°C";
+    thirdDayMax.innerHTML = Math.round(data[3].max) + "°C";
+    thirdDayMin.innerHTML = Math.round(data[3].min) + "°C";
     thirdDayIcon.src = "http://openweathermap.org/img/w/" + data[3].weatherIcon + ".png";
 
     var fourthDayIcon = document.getElementById('fourthWeatherIcon');
     var fourthDayMax = document.getElementById('fourthMax');
     var fourthDayMin = document.getElementById('fourthMin');
-    fourthDayMax.innerHTML = data[4].max + "°C";
-    fourthDayMin.innerHTML = data[4].min + "°C";
+    fourthDayMax.innerHTML = Math.round(data[4].max) + "°C";
+    fourthDayMin.innerHTML = Math.round(data[4].min) + "°C";
     fourthDayIcon.src = "http://openweathermap.org/img/w/" + data[4].weatherIcon + ".png";
 
     var fifthDayIcon = document.getElementById('fifthWeatherIcon');
     var fifthDayMax = document.getElementById('fifthMax');
     var fifthDayMin = document.getElementById('fifthMin');
-    fifthDayMax.innerHTML = data[5].max + "°C";
-    fifthDayMin.innerHTML = data[5].min + "°C";
+    fifthDayMax.innerHTML = Math.round(data[5].max) + "°C";
+    fifthDayMin.innerHTML = Math.round(data[5].min) + "°C";
     fifthDayIcon.src = "http://openweathermap.org/img/w/" + data[5].weatherIcon + ".png";
 
     var sixthDayIcon = document.getElementById('sixthWeatherIcon');
     var sixthDayMax = document.getElementById('sixthMax');
     var sixthDayMin = document.getElementById('sixthMin');
-    sixthDayMax.innerHTML = data[6].max + "°C";
-    sixthDayMin.innerHTML = data[6].min + "°C";
+    sixthDayMax.innerHTML = Math.round(data[6].max) + "°C";
+    sixthDayMin.innerHTML = Math.round(data[6].min) + "°C";
     sixthDayIcon.src = "http://openweathermap.org/img/w/" + data[6].weatherIcon + ".png";
 
 
     var seventhDayIcon = document.getElementById('seventhWeatherIcon');
     var seventhDayMax = document.getElementById('seventhMax');
     var seventhDayMin = document.getElementById('seventhMin');
-    seventhDayMax.innerHTML = data[7].max + "°C";
-    seventhDayMin.innerHTML = data[7].min + "°C";
+    seventhDayMax.innerHTML = Math.round(data[7].max) + "°C";
+    seventhDayMin.innerHTML = Math.round(data[7].min) + "°C";
     seventhDayIcon.src = "http://openweathermap.org/img/w/" + data[7].weatherIcon + ".png";
 
 }
